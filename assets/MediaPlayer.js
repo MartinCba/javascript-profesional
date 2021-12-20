ccabb27c4737585c7ca3353a45ae9701e026a80d
Martín Hernandez <martinalejandrohernandez1@gmail.com>
2021-12-20 17:01:11 -0300

    primer plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,9 +1,26 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
 
 MediaPlayer.prototype.play = function () {
   this.media.play();
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
 };
 
 MediaPlayer.prototype.pause = function () {
