68e82726a66225b540e06e0e7520f18873f3377e
MartinCba <martinalejandrohernandez1@gmail.com>
2022-01-03 16:37:34 -0300

    Clases

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,9 @@
+import MediaPlayer from '../MediaPlayer';
+
 class AutoPause {
+  private threshold: number;
+  player: MediaPlayer;
+
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
@@ -16,7 +21,7 @@ class AutoPause {
     document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
 
-  handleIntersection(entries) {
+  private handleIntersection(entries: IntersectionObserverEntry[]) {
     const entry = entries[0];
 
     const isVisible = entry.intersectionRatio >= this.threshold;
@@ -27,7 +32,7 @@ class AutoPause {
       this.player.pause();
     }
   }
-  handleVisibilityChange() {
+  private handleVisibilityChange() {
     const isVisible = document.visibilityState === "visible";
     if (isVisible) {
       this.player.play();
